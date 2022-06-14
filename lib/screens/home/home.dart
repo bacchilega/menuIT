a0a39be8017cec05ed1836646c948759fe107141
bacchilega <lfbacchilega@gmail.com>
2022-06-14 10:20:17 +0200

    new screens test

---
+++ lib/screens/home/home.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {}
+}
---
+++ lib/screens/home/wrapper.dart
@@ -0,0 +1,13 @@
+import 'home.dart';
+import 'package:flutter/material.dart';
+
+class Wrapper extends StatelessWidget {
+  const Wrapper({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+
+    // return home or authenticate
+    return Home()
+  }
+}
